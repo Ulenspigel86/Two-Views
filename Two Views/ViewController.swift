9b05815743bf52f818cda35c902af689aafd164f
Ulenspigel86 <66313198+Ulenspigel86@users.noreply.github.com>
2024-10-02 13:23:37 +0300

    Initial Commit

---
+++ Two Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Views
+//
+//  Created by Артем Иванов on 02.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
